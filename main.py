b059ba9c13f515db16af8b10a19a1a87e0aa3065
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2021-11-15 17:08:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 print(cv2.__version__)
+YELLOW_LOW  = np.asarray([20, 100, 100])
+YELLOW_HIGH = np.asarray([30, 255, 255])
+WHITE_LOW   = np.asarray([0, 0, 230])
+WHITE_HIGH  = np.asarray([255, 80, 255])
 
 ## image read
-img = cv2.imread('lane_l.png')
-img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+img = cv2.imread('lane.png')
+#img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 img = cv2.GaussianBlur(img, (3,3),0) #일부러 가우시안 입히기
-plt.imshow(img, cmap='gray')
-plt.show()
+cv2.imshow('img',img)
+
+## csv 변환
+def color_extraction(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    yellow_mask = cv2.inRange(hsv, YELLOW_LOW, YELLOW_HIGH)
+    white_mask = cv2.inRange(hsv, WHITE_LOW, WHITE_HIGH)
+    mask = cv2.bitwise_or(yellow_mask, white_mask)
+    # cv2.imshow('',threshold)
+
+    return cv2.bitwise_and(img, img, mask=mask)
+
+extracted_img = color_extraction(img)
+cv2.imshow('extracted',extracted_img)
+
 ## Edge Detection
 
 def region_interest(edge):
@@ -33,24 +51,25 @@ def region_interest(edge):
     cv2.fillPoly(mask, region_of_interest_vertices, ignore_mask_color)
     return cv2.bitwise_and(edge, mask)
 
-edge = cv2.Canny(img, 50, 200)
+edge = cv2.Canny(extracted_img, 50, 200)
 plt.imshow(edge, cmap='gray')
 mask = region_interest(edge)
 plt.imshow(mask, cmap='gray')
 plt.show()
 
-## hough line detect
-def draw_lines(img, lines, color=[255,0,0], thickness=5):
+## houghp line detect
+'''
+def draw_linesp(img, lines, color=[0,0,255], thickness=2):
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(img,(x1,y1), (x2, y2), color, thickness)
 
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+def houghp_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),
                             minLineLength=min_line_len,
                             maxLineGap=max_line_gap)
     line_imge = np.zeros((img.shape[0], img.shape[1],3), dtype=np.uint8)
-    draw_lines(line_imge, lines)
+    draw_linesp(line_imge, lines)
     return line_imge
 
 rho = 2
@@ -59,10 +78,33 @@ threshold = 20
 min_line_len = 50
 max_line_gap = 150
 
-lines = cv2.cvtColor(hough_lines(mask, rho, theta, threshold, min_line_len, max_line_gap), cv2.COLOR_RGB2GRAY)
+lines = houghp_lines(mask, rho, theta, threshold, min_line_len, max_line_gap)
 
-plt.imshow(lines, cmap='gray')
-plt.show()
+cv2.imshow('hough',lines)
+'''
+## original hough line
+def draw_lines(img, lines, color=[0,0,255], thickness=2):
+    for line in lines:
+        rho, theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+
+        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(mask, origin, threshold):
+    lines = cv2.HoughLines(mask, 1, np.pi / 180, threshold)
+    line_imge = origin#np.zeros((img.shape[0], img.shape[1],3), dtype=np.uint8)
+    draw_lines(line_imge, lines)
+    return line_imge
+
+lines =hough_lines(mask, img, 55)
+cv2.imshow('hough',lines)
 
 ## R-L search
 
